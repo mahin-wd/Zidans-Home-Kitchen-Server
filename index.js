23b8779c7eb48a81ed4541ed2867dab16c16b439
mahin-wd <wdmahin.mhm@gmail.com>
2022-11-07 22:27:33 +0600

    added mongodb and loaded data

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+require ('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
+
+//* mongodb....
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4lqljgn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async  function run() {
+    try {
+        const itemCollection = client.db('zidansKitchen').collection('items');
+        app.get('/food-items', async(req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const foods = await cursor.toArray();
+            const count = await itemCollection.estimatedDocumentCount();
+            res.send({count, foods});
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
